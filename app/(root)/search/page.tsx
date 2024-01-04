691754117b5044182bc3f810d29700ac5bab4284
NotAdhiraj <adhiajsingh129@gmail.com>
2024-01-04 14:30:17 +0530

    ignoreTsErrors

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,12 +1,6 @@
-import PostThread from "@/components/forms/PostThread";
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import ThreadsTab from "@/components/shared/ThreadsTab";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { profileTabs } from "@/constants";
-import { fetchUser } from "@/lib/actions/user.action";
+import UserCard from "@/components/cards/UserCard";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.action";
 import { currentUser } from "@clerk/nextjs";
-import { profile } from "console";
-import Image from "next/image";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
@@ -18,11 +12,37 @@ const Page = async () => {
 
 	if (!userInfo?.onboarded) redirect("/onboarding");
 
-   //Fetch Users
-
+	//Fetch Users
+	const result = await fetchUsers({
+		userId: user.id,
+		searchString: "",
+		pageNumber: 1,
+		pageSize: 25,
+		sortBy: "desc",
+	});
 	return (
 		<section>
 			<h1 className='head-text mb-10'>Search</h1>
+
+			{/* {Search bar} */}
+			<div className='mt-14 flex flex-col gap-9'>
+				{result.users.length === 0 ? (
+					<p className='no-result'>No User Found</p>
+				) : (
+					<>
+						{result.users.map((person) => (
+							<UserCard
+								key={person.id}
+								id={person.id}
+								name={person.name}
+								username={person.name}
+								imgUrl={person.image}
+								personType='User'
+							/>
+						))}
+					</>
+				)}
+			</div>
 		</section>
 	);
 };
